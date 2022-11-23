9ae249411e806671fdf467de29845680da66a3c3
onkar-git <54213633+onkar-git@users.noreply.github.com>
2022-11-23 15:31:56 +0530

    Update mongodb.py

--- database/mongodb.py
+++ database/mongodb.py
@@ -8,7 +8,7 @@ ca = certifi.where()
 class MongodbOperation:
 
     def __init__(self) -> None:
-        #MONGO_DB_URL="mongodb+srv://onkarsensor:onkarmongo@cluster0.vwftfwy.mongodb.net/?retryWrites=true&w=majority"
+        
         self.client = pymongo.MongoClient(os.getenv('MONGO_DB_URL'),tlsCAFile=ca)
         #self.client = pymongo.MongoClient('MONGO_DB_URL',tlsCAFile=ca)
         self.db_name="ineuronabc"
